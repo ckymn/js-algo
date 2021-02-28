01b1181093eeccbeefca1a8d3024c82cb9537595
ckymn <cokyamanmuhammet@gmail.com>
2021-02-28 18:47:12 +0300

    1.q.2

--- 1.question.js
+++ 1.question.js
@@ -1,15 +1,29 @@
 function halv_array_swap(arr) {
+  let new_arr = [];
   if (arr.length % 2 != 0) {
-    return false;
-  }
+    new_arr.push(arr.pop());
 
-  for (var i = 0; i < arr.length / 2; i++) {
-    var tmp = arr[i];
-    arr[i] = arr[i + arr.length / 2];
-    arr[i + arr.length / 2] = tmp;
+    for (let i = 0; i < new_arr.length / 2; i++) {
+      let tmp = new_arr[i];
+      new_arr[i] = new_arr[i + new_arr.length / 2];
+      new_arr[i + new_arr.length / 2] = tmp;
+    }
   }
-  return arr;
+  if (arr.length % 2 == 0) {
+    for (let i = 0; i < arr.length / 2; i++) {
+      let tmp = arr[i];
+      arr[i] = arr[i + arr.length / 2];
+      arr[i + arr.length / 2] = tmp;
+    }
+    return arr;
+  }
+  if (typeof arr == string) 
+    throw new Error("please enter a array");
+
+  return new_arr;
 }
 
-console.log(halv_array_swap([1, 2, 3, 4, 5, 6])); //true
-  console.log(halv_array_swap([1,2,3,4,5,6,7])) //false
+// console.log(halv_array_swap([1, 2, 3, 4, 5, 6])); //[ 4, 5, 6, 1, 2, 3 ]
+// console.log(halv_array_swap([1, 2, 3, 4, 5, 6, 7])); //[ 4, 5, 6, 1, 2, 3 ]
+// console.log(halv_array_swap(["1","2","3","4","5","6"]));//[ '4', '5', '6', '1', '2', '3' ]
+// console.log(halv_array_swap("muhamemt")) // please enter a array 
