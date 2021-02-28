b1447c401a2b752619580aaab743c4417ecf792e
ckymn <cokyamanmuhammet@gmail.com>
2021-02-28 19:23:52 +0300

    2.q-1

---
+++ 2.question.js
@@ -0,0 +1,23 @@
+// js-sort
+
+const sort = (a) => {
+  let swap;
+  let n = a.length-1 ;
+
+  do {
+    swap = false;
+    for (let i = 0; i < n; i++) {
+      if (a[i] < a[i + 1]) {
+        let temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+        swap = true;
+      }
+    }
+    n--;
+    console.log(swap);
+} while (swap)
+  return a;
+};
+
+console.log(sort([12, 345, 32, 2, 5252, 222, 11111, 636346, 32, 1]));
